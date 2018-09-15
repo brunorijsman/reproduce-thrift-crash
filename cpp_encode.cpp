f57185d1c70c5f5043034ddbbbc90bab97872627
Bruno Rijsman <brunorijsman@gmail.com>
2018-09-15 13:57:12 -0700

    Starting

---
+++ cpp_encode.cpp
@@ -0,0 +1,6 @@
+#include "gen-cpp/model_types.h"
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
---
+++ model.thrift
@@ -0,0 +1,13 @@
+struct Person {
+    1: required i32 age;
+}
+
+
+//struct Coordinate {
+//    1: required i32 x;
+//    2: required i32 y;
+//}
+
+//struct Map {
+//    1: required map<Coordinate, string> places;
+//}
