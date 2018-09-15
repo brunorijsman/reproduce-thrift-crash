81123fb9029a8ae26c593a1c81b6b5d4206a956d
Bruno Rijsman <brunorijsman@gmail.com>
2018-09-15 14:22:21 -0700

    Update reproduce.sh

--- reproduce.sh
+++ reproduce.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+
+sudo yum -y group install 'Development Tools'
+
+sudo yum -y install \
+    ant \
+    autoconf \
+    automake \
+    bison \
+    byacc \
+    boost \
+    flex \
+    git \
+    java-1.7.0-openjdk \
+    libevent \
+    libevent-devel \
+    libtool \
+    make \
+    openssl-devel \
+    python-devel \
+    wget
+
+git clone https://github.com/apache/thrift.git
+cd thrift
+./bootstrap.sh
+./configure --with-lua=no
+make
+sudo make install
