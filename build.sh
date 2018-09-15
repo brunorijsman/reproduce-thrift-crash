9665396b17a8aca31cfa766f84a8fd7447fc4b5f
Bruno Rijsman <brunorijsman@gmail.com>
2018-09-15 14:34:08 -0700

    Progress

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Assuming you already did the following to install and run this script:
+#
+# cd ~
+# sudo yum -y install git
+# git clone https://github.com/brunorijsman/reproduce-thrift-crash.git
+# cd reproduce-thrift-crash.git
+# ./build.sh
+
 sudo yum -y group install 'Development Tools'
 
 sudo yum -y install \
@@ -8,7 +16,6 @@ sudo yum -y install \
     automake \
     bison \
     byacc \
-    boost \
     flex \
     git \
     java-1.7.0-openjdk \
@@ -20,6 +27,14 @@ sudo yum -y install \
     python-devel \
     wget
 
+cd ~
+wget http://jaist.dl.sourceforge.net/project/boost/boost/1.45.0/boost_1_45_0.tar.gz
+tar xzf boost_1_45_0.tar.gz
+cd boost_1_45_0
+./bootstrap.sh 
+sudo ./bjam install
+
+cd ~
 git clone https://github.com/apache/thrift.git
 cd thrift
 ./bootstrap.sh
