d056920f981e55c7b754fbe97385ea659b2fa5f5
Bruno Rijsman <brunorijsman@gmail.com>
2018-09-17 15:37:29 -0700

    Progress

--- cpp_encode.cpp
+++ cpp_encode.cpp
@@ -1,5 +1,18 @@
 #include "gen-cpp/model_types.h"
 
+bool Coordinate::operator<(const Coordinate& other) const
+{
+    if (x < other.x) {
+        return true;
+    } else if (x > other.x) {
+        return false;
+    } else if (y < other.y) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     Coordinate coordinate;
