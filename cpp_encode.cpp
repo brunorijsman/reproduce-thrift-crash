13cfe8465c223603e6aa2d997adb33f96b2ccd68
Bruno Rijsman <brunorijsman@gmail.com>
2018-09-17 15:55:52 -0700

    Progress

--- cpp_encode.cpp
+++ cpp_encode.cpp
@@ -15,13 +15,25 @@ bool Coordinate::operator<(const Coordinate& other) const
     }
 }
 
-int main(int argc, char const *argv[])
+void add_sample_to_terrain(Terrain& terrain, int32_t x, int32_t y, int32_t altitude)
 {
     Coordinate coordinate;
+    coordinate.x = x;
+    coordinate.y = y;
+    std::pair<Coordinate, int32_t> sample(coordinate, altitude);
+    terrain.altitude_samples.insert(sample);
+}
+
+void encode_terrain_to_file()
+{
     Terrain terrain;
+    add_sample_to_terrain(terrain, 10, 10, 100);
+    add_sample_to_terrain(terrain, 20, 20, 200);
+    add_sample_to_terrain(terrain, 30, 30, 300);
+}
 
-    coordinate.x = 10
-    coordinate.y 
-
+int main(int argc, char const *argv[])
+{
+    encode_terrain_to_file()
     return 0;
 }
